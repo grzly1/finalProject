608d3855f0d1bc445b5ec7bca3e58fb343079438
grzly1 <matthollingsworth@Mattss-MacBook-Air.local>
2016-12-12 16:48:35 -0700

    personal website

---
+++ main.js
@@ -0,0 +1,17 @@
+// $('.content-section-a').tubular({
+//     videoId: '0Bmhjf0rKe8'
+// });
+
+$(document).ready(function() {
+    $('#wrapper').tubular({
+        videoId: '0Bmhjf0rKe8'
+    });
+    // where idOfYourVideo is the YouTube ID.
+});
+
+
+// $(document).ready(function() {
+//     $('#wrapper').tubular({
+//         videoId: 'idOfYourVideo'
+//     }); // where idOfYourVideo is the YouTube ID.
+// });
